a9274d7465101e53488d3cf59fed26d98d5008cc
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-12-15 19:43:07 +0530

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,47 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-      <div>
-      <h1>Navbar</h1>
-      
-      </div>
-  )
+    <nav className="nav">
+      <a href="/" className="site-title">
+        Medical Darpan
+      </a>
+
+      <ul>
+        <li>
+          <a href="/home" className="site-routes">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/products" className="site-routes">
+            Products
+          </a>
+        </li>
+        <li>
+          <a href="/distributors" className="site-routes">
+            Distributors
+          </a>
+        </li>
+        <li>
+          <a href="/manufacturers" className="site-routes">
+            Manufacturers
+          </a>
+        </li>
+        <li>
+          <a href="/aboutus" className="site-routes">
+            About us
+          </a>
+        </li>
+        <li>
+          <a href="/blog" className="site-routes">
+            Blog
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
