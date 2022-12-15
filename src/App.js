babaf5702e2cfe0bc27ba488ca1aabddd286be9e
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-12-15 19:19:29 +0530

    Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Header />
+      <Navbar />
+      <Sidebar />
     </div>
   );
 }
