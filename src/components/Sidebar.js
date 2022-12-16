9a424199e963783359453c7de34e3c016ff22a54
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-12-16 11:56:09 +0530

    Sidebar complete

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarItem from "./SidebarItem";
+import items from "../data/sidebar.json";
 
 function Sidebar() {
   return (
-    <div className="sidebar"> 
-      <SidebarItem />
+    <div className="sidebar">
+      {items.map((item, index) => (
+        <SidebarItem key={index} item={item} />
+      ))}
     </div>
   );
 }
--- src/components/SidebarItem.js
+++ src/components/SidebarItem.js
@@ -2,18 +2,26 @@ import React, { useState } from "react";
 import "./SidebarItem.css";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
-function SidebarItem() {
+function SidebarItem({ item }) {
+  console.log(item);
   const [open, setOpen] = useState(true);
   return (
     <div className={open ? "sidebar-item open" : "sidebar-item"}>
       <div className="sidebar-title">
-        <span>Related Category</span>
-        <ArrowDropDownIcon
-          className="toggle-btn"
-          onClick={() => setOpen(!open)}
-        />
+        <span>{item.title}</span>
+        {item.childrens && (
+          <ArrowDropDownIcon
+            className="toggle-btn"
+            onClick={() => setOpen(!open)}
+          />
+        )}
       </div>
-      <div className="sidebar-list">hello</div>
+      <div className="sidebar-list">
+        {item.childrens &&
+          item.childrens.map((child, index) => (
+            <SidebarItem key={index} item={child} />
+          ))}
+      </div>
     </div>
   );
 }
