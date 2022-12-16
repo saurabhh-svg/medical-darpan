39e1741e8aa657414997842eb3c85667f9e5279d
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-12-16 15:07:07 +0530

    Home page edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
@@ -9,8 +9,12 @@ import Distributors from "./pages/Distributors";
 import Manufacturers from "./pages/Manufacturers";
 import Aboutus from "./pages/Aboutus";
 import Blog from "./pages/Blog";
+import Cards from "./components/Cards";
+import Sidebar from "./components/Sidebar";
+import { Wrap } from "@chakra-ui/react";
 
 function App() {
+  const [data, setData] = useState([1, 2, 3, 4, 5, 6]);
   return (
     <div className="container">
       <Navbar />
@@ -24,6 +28,19 @@ function App() {
       </Routes>
 
       <Header />
+      <div className="super-container">
+        <div className="sidebar-container">
+          <Sidebar />
+        </div>
+
+        <div className="card-container">
+          <Wrap spacing="40px" justify="center" marginTop="30px">
+            {data.map((item) => (
+              <Cards />
+            ))}
+          </Wrap>
+        </div>
+      </div>
     </div>
   );
 }
